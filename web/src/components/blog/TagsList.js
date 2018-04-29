1f4c02fd93a62d3401876753b8d347407527c14f
Aceysx <aceysx@163.com>
2018-04-29 15:08:18 +0800

    add tag count

--- web/src/components/blog/TagsList.js
+++ web/src/components/blog/TagsList.js
@@ -3,6 +3,7 @@ import { AppBar, Button, Toolbar, Typography } from "material-ui/es/index";
 import * as Color from '../../constant/color'
 import MenuIcon from 'material-ui-icons/Menu'
 import Tag from '../common/blog/Tag'
+import Badge from 'material-ui/Badge';
 
 export default class TagsList extends React.Component {
 
@@ -10,13 +11,17 @@ export default class TagsList extends React.Component {
     const defaultTagId = this.props.defaultTagId
     const selectedBgColor = '#8D6E63'
     const tagsList = this.props.tags.map(item => {
+      if (!item.id) {
+        return
+      }
       return <span style={{ display: 'inline-block', height: 30 }}
         onClick={() => this.props.switchTag(item.id)}>
+
         <Tag bgColor={defaultTagId === item.id ? selectedBgColor : item.bgColor}
-          content={item.content} />
+          content={item.content + ' | ' + item.blogs.length} />
       </span>
     })
-    
+
     return <div style={{ width: '270%' }}>
       <AppBar position='static' style={{ background: Color.BLUE, marginBottom: 10 }}>
         <Toolbar>
