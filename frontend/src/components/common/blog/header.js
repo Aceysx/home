675ba66153a6984a2627a3e62acca04cb729aec0
Aceysx <aceysx@163.com>
2018-04-30 12:12:08 +0800

    add icon

--- frontend/src/components/common/blog/header.js
+++ frontend/src/components/common/blog/header.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import BackgroundImage from '../../../constant/images/bg.jpg'
-import APPBar from '../app-bar'
+import { Icon } from 'antd'
+import '../../../css/header.css'
 
-const Header = (props) => {
-  const bgStyle = {
-    width: '100%',
-    height: props.height || 50,
-    backgroundImage: `url(${BackgroundImage})`,
-    backgroundSize: '100%',
-    marginBottom: 50
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: false,
+      collapsedIcon: this.state.collapsed ? 'menu-fold' : 'menu-unfold'
+    }
   }
-  return <div style={bgStyle}>
-    <APPBar />
-    <div style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}}>
-      {props.title}
+  render() {
+    let header_style = {
+      height: '400px',
+      background: 'rgb(33, 150, 243,.95)'
+    }
+    let header_style_top = {
+      position: 'fixed',
+      height: '50px',
+      width: '100%',
+      top: 0,
+      background: 'rgb(33, 150, 243)'
+    }
+
+    return <div style={header_style}>
+      <div style={header_style_top}>
+        <Icon className='header-icon header-icon-open' type={this.state.collapsedIcon} />
+        <Icon className='header-icon header-icon-search' type="search" />
+      </div>
     </div>
-  </div>
+  }
 }
 
 export default Header
